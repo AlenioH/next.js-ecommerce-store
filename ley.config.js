620635a0b7348a233e7005ba3d32c55e4dda4fe9
Alena Hasslacher <alena.hasslacher@gmail.com>
2020-06-16 17:01:13 +0200

    Add confuguration for heroku deploy

---
+++ ley.config.js
@@ -0,0 +1,9 @@
+require('./extractHerokuDatabaseEnvVars')();
+
+const options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.ssl = { rejectUnauthorized: false };
+}
+
+module.exports = options;
